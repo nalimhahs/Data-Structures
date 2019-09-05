6296e03e6a50820b6ce1c7a7d7cfe5daa7264001
parthsuresh <parthsuresh96@gmail.com>
2019-09-05 16:15:36 +0530

    Started linked list

---
+++ linked-list/ll.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+typedef struct node
+{
+    int data;
+    struct node *link;
+} node;
+
+node *header;
+
+headerlink = NULL;
+
+void insert(){
+
+}
+
+void read(){
+
+}
+
+void delete(){
+
+}
+
+
+void main(){
+
+}
