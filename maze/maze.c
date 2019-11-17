f5fd9cb1f5d321e41fb50744909096256093dd93
Milan Shah <milanshah1@gmail.com>
2019-11-17 15:47:59 +0530

    Finished maze

--- maze/maze.c
+++ maze/maze.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#define N 10
+#define N 3
 #define MAX N *N
 
 typedef struct coordinate
@@ -37,7 +37,6 @@ move pop()
     if (top < 0)
     {
         printf("\nUnderflow\n");
-        return;
     }
     else
     {
@@ -79,11 +78,11 @@ int calc_diff(point start, point end)
     return x + y;
 }
 
-point *get_possible_moves(point current)
+point *get_possible_moves(point current, point *possible)
 {
-    point possible[4];
+    // point possible[4];
     possible[0].x = current.x + 1;
-    possible[0].y = current.x;
+    possible[0].y = current.y;
 
     possible[1].x = current.x;
     possible[1].y = current.y + 1;
@@ -97,9 +96,10 @@ point *get_possible_moves(point current)
     return possible;
 }
 
-point *get_optimized_moves(point current, point end)
+point *get_optimized_moves(point current, point end, point *possible_moves)
 {
-    point possible_moves[4] = get_possible_moves(current);
+    // point possible_moves[4];
+    get_possible_moves(current, possible_moves);
     point temp;
     int small;
 
@@ -146,7 +146,8 @@ int get_next_move(int maze[N][N], point end)
 
     move next;
     int i = current.direction;
-    point possible_moves[4] = get_optimized_moves(current.position, end);
+    point possible_moves[4];
+    get_optimized_moves(current.position, end, possible_moves);
     while (!is_valid(possible_moves[i], maze) && i < 4)
     {
         i++;
@@ -158,6 +159,8 @@ int get_next_move(int maze[N][N], point end)
     if (i == 3)
     {
         current = pop();
+        current.direction++;
+        push(current);
         get_next_move(maze, end);
     }
     else
@@ -166,6 +169,7 @@ int get_next_move(int maze[N][N], point end)
         push(current);
         next.position = possible_moves[i];
         next.direction = 0;
+        push(next);
         get_next_move(maze, end);
     }
 }
@@ -181,7 +185,7 @@ void print_solution(int maze[N][N])
         {
             if (maze[i][j])
             {
-                sol[i][j] = '0';
+                sol[i][j] = '1';
             }
             else
             {
@@ -195,6 +199,35 @@ void print_solution(int maze[N][N])
         current = pop();
         sol[current.position.x][current.position.y] = '*';
     }
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            printf("%c ", sol[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void print_maze(int maze[N][N])
+{
+    char sol[N][N];
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            if (maze[i][j])
+            {
+                sol[i][j] = '1';
+            }
+            else
+            {
+                sol[i][j] = '-';
+            }
+        }
+    }
+
     for (int i = 0; i < N; i++)
     {
         for (int j = 0; j < N; j++)
@@ -226,6 +259,8 @@ int solve(int maze[N][N], point current, point end)
 void main()
 {
     int maze[N][N], x, y;
+    point start, end;
+    char ch;
     for (int i = 0; i < N; i++)
     {
         for (int j = 0; j < N; j++)
@@ -240,5 +275,23 @@ void main()
         scanf("%d", &x);
         printf("\nEnter barrier Y position: ");
         scanf("%d", &y);
-    }while();
+        maze[x][y] = 1;
+        printf("\nDo you want to add another barrier?(y/N): ");
+        scanf("%c", &ch);
+        scanf("%c", &ch);
+    } while (ch == 'y' || ch == 'Y');
+
+    print_maze(maze);
+
+    printf("\nEnter start X position: ");
+    scanf("%d", &start.x);
+    printf("\nEnter start Y position: ");
+    scanf("%d", &start.y);
+
+    printf("\nEnter end X position: ");
+    scanf("%d", &end.x);
+    printf("\nEnter end Y position: ");
+    scanf("%d", &end.y);
+
+    solve(maze, start, end);
 }
